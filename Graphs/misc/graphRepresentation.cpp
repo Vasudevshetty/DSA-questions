828cc4fe0ad2ba5453b731b202cc4aee98cd0eb8
Vasudevshetty <vasudeepu2815@gmail.com>
2024-03-17 10:26:30 +0530

    learnt how to represent grpahs in adj mat and list methods

--- Graphs/misc/graphRepresentation.cpp
+++ Graphs/misc/graphRepresentation.cpp
@@ -1,24 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void display(vector<vector<int>> &mat){
-    for (size_t i = 1; i < mat.size(); i++){
-        for (size_t j = 1; j < mat[i].size(); j++){
-            cout << mat[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-int main(){
+int main()
+{
     int vertex, edges;
     cin >> vertex >> edges;
-    vector<vector<int>> graph(vertex + 1, vector<int>(vertex + 1, 0));
-    for (int i = 0; i < edges; i++){
+    vector<vector<int>> graph(vertex + 1, vector<int>(edges + 1, 0)); // adj matrix
+    vector<vector<int>> graphList(vertex + 1);                        // adj list
+    for (int i = 0; i < edges; i++)
+    {
         int u, v;
         cin >> u >> v;
-        graph[u][v] = 1;
+
+        /*
+            graph[u][v] = graph[v][u] = 1; // to store the adj matrix
+        */
+
+        graph[u].push_back(v); // to store the adj list
+        graph[v].push_back(u);
     }
-        display(graph);
     return 0;
 }
