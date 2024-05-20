e38db30eb31ce9200fbb73047a66f7d210d25c85
Vasudevshetty <vasudeepu2815@gmail.com>
2024-05-21 00:27:59 +0530

    Solved binarysearch searching the element in a rotated sorted array.

---
+++ Binary-Search/searchinrotated.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int searchInRotated(vector<int> &arr, int key)
+{
+    int low = 0, high = arr.size() - 1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+
+        if (arr[low] <= arr[mid])
+        {
+
+            if (arr[low] <= key && key <= arr[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        else
+        {
+            if (arr[mid] <= key && key <= arr[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr;
+    for (int i = 0; i < n; i++)
+    {
+        int ele;
+        cin >> ele;
+        arr.push_back(ele);
+    }
+    int key;
+    cin >> key;
+    cout << searchInRotated(arr, key) << endl;
+    return 0;
+}
